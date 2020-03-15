885dba671f2f95d02b6c600c9d3d6ded270b6b66
Yuxuan Zhao <me@yuxuanz.com>
2020-03-14 23:58:13 -0600

    add the backend code

--- sockets.py
+++ sockets.py
@@ -26,6 +26,26 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+# Reference: https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/chat.py
+clients = list()
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
 class World:
     def __init__(self):
         self.clear()
@@ -62,6 +82,9 @@ class World:
 myWorld = World()        
 
 def set_listener( entity, data ):
+    msg = json.dumps({entity: data})
+    for client in clients:
+        client.put(msg)
     ''' do something with the update ! '''
 
 myWorld.add_set_listener( set_listener )
@@ -72,16 +95,34 @@ def hello():
     return redirect(url_for('static', filename='index.html'))
 
 def read_ws(ws,client):
-    '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            print("WS RECV: %s" % msg)
+            if (msg is not None):
+                packet = json.loads(msg)
+                send_all_json( packet )
+            else:
+                break
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
-    '''Fufill the websocket URL of /subscribe, every update notify the
-       websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )    
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
